f2bac5ec8a0c520c14c121e5d6b65f138d68c48b
jspr-rnty-jr <jasperjr@rantay@gmail.com>
2024-09-13 19:13:36 +0800

    added react-particles-js for better design, and fixed fontsizes responsiveness

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,23 @@
+import ParticlesBg from 'particles-bg'
+
 const SideBar = (props) => {
     const {handleToggleModal, data} = props;
 
     return ( 
-        <div className="sidebar">
-        <div className="bgOverlay" onClick={handleToggleModal}></div>
+    <div className="sidebar">
+        <div className="bgOverlay" onClick={handleToggleModal}>
+            <ParticlesBg color="#ffffff" num={30} type="cobweb" bg={true} />
+        </div>
         <div className="sidebarContents">
             <h2>{data?.title}</h2>
+            <p className="descriptionDate">{data?.date}</p>
             <div className="descriptionContainer">
-                <p className="descriptionTitle">{data?.date}</p>
-                <p>{data?.explanation}</p>
+                <p className="descriptionExplanation"> {data?.explanation}</p>
             </div>
             <button onClick={handleToggleModal}>
                 <i className="fa-solid fa-arrow-right"></i>
             </button>
+            <ParticlesBg color="#ffffff" num={10} type="cobweb" bg={true} />
         </div>
     </div>
      );
